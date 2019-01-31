5c4a3195177a924cbbfa86e70f78188abe42bf88
marinakulsha <marina_kul@tut.by>
2019-01-31 17:42:53 +0300

    init Project

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import './js/carousel.js';
+import './scss/base.scss';
+import './scss/carousel-style.scss';
+import './scss/header.scss';
+import './scss/footer.scss';
+import './scss/contact-form.scss';
+import './scss/equipment.scss';
